26a0a45c4684c39c17918b11583b1d7926c96ec5
Jesse Beck <thewebbeckons@users.noreply.github.com>
2020-08-17 16:32:23 -0400

    added paginationjs

--- src/js/dataset.js
+++ src/js/dataset.js
@@ -57,6 +57,120 @@ var reportData = [
         ]
     },
     {
+      "cover": "images/cover1.jpg",
+      "type": "Annual Report",
+      "title": "2019 Annual Report",
+      "documents": [
+          {
+              "file_size": "16 KB",
+              "file_type": "PDF",
+              "url": "files/test.pdf",
+              "title": "2019 Annual Report"
+          },
+          {
+              "file_size": "7 KB",
+              "file_type": "PDF",
+              "url": "files/test.pdf",
+              "title": "2019 Proxy Statement"
+          }
+      ]
+  },
+  {
+      "cover": "images/cover2.jpg",
+      "type": "Annual Report",
+      "title": "2018 Annual Report",
+      "documents": [
+          {
+              "file_size": "16 KB",
+              "file_type": "PDF",
+              "url": "files/test.pdf",
+              "title": "2018 Annual Report"
+          },
+          {
+              "file_size": "7 KB",
+              "file_type": "PDF",
+              "url": "files/test.pdf",
+              "title": "2018 Proxy Statement"
+          }
+      ]
+  },
+  {
+      "cover": "images/cover3.jpg",
+      "type": "Annual Report",
+      "title": "2017 Annual Report",
+      "documents": [
+          {
+              "file_size": "16 KB",
+              "file_type": "PDF",
+              "url": "files/test.pdf",
+              "title": "2017 Annual Report"
+          },
+          {
+              "file_size": "7 KB",
+              "file_type": "PDF",
+              "url": "files/test.pdf",
+              "title": "2017 Proxy Statement"
+          }
+      ]
+  },
+  {
+    "cover": "images/cover1.jpg",
+    "type": "Annual Report",
+    "title": "2019 Annual Report",
+    "documents": [
+        {
+            "file_size": "16 KB",
+            "file_type": "PDF",
+            "url": "files/test.pdf",
+            "title": "2019 Annual Report"
+        },
+        {
+            "file_size": "7 KB",
+            "file_type": "PDF",
+            "url": "files/test.pdf",
+            "title": "2019 Proxy Statement"
+        }
+    ]
+},
+{
+    "cover": "images/cover2.jpg",
+    "type": "Annual Report",
+    "title": "2018 Annual Report",
+    "documents": [
+        {
+            "file_size": "16 KB",
+            "file_type": "PDF",
+            "url": "files/test.pdf",
+            "title": "2018 Annual Report"
+        },
+        {
+            "file_size": "7 KB",
+            "file_type": "PDF",
+            "url": "files/test.pdf",
+            "title": "2018 Proxy Statement"
+        }
+    ]
+},
+{
+    "cover": "images/cover3.jpg",
+    "type": "Annual Report",
+    "title": "2017 Annual Report",
+    "documents": [
+        {
+            "file_size": "16 KB",
+            "file_type": "PDF",
+            "url": "files/test.pdf",
+            "title": "2017 Annual Report"
+        },
+        {
+            "file_size": "7 KB",
+            "file_type": "PDF",
+            "url": "files/test.pdf",
+            "title": "2017 Proxy Statement"
+        }
+    ]
+},
+    {
         "cover": "images/cover1.jpg",
         "type": "Annual Report",
         "title": "2016 Annual Report",
--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,17 @@
 var reportsWidget = {
     options: {
         containerSelector: '.reports',
+        paginationSelector: '#pagination-container',
         template: (
             '{{#.}}' +
                 '<article class="reports_item">' +
                     '<a href="{{cover}}" target="_blank">' +
-                        '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>'
+                        '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>' +
                     '</a>' +
                     '<footer class="reports_docs">' +
                         '{{#documents}}' +
                             '<h3 class="reports_title">' +
-                                '<a href="{{url}}" target="_blank">{{title}}</a>' +
+                            '<a href="{{url}}" target="_blank">{{title}} <span>({{file_size}} {{file_type}})</span></a>' +
                             '</h3>' +
                         '{{/documents}}' +
                     '</footer>' +
@@ -20,14 +21,19 @@ var reportsWidget = {
     },
 
     init: function() {
-        this.renderReports(reportData || []);
+        this.renderReports(reportData || []);        
     },
-
     renderReports: function(reports) {
         var inst = this,
             options = inst.options;
-
-        $(options.containerSelector).html(Mustache.render(options.template, reports));
+        $(options.paginationSelector).pagination({
+          dataSource: reports,
+          pageSize: 9,
+          callback: function(data, pagination) {
+            var html = Mustache.render(options.template, data);
+            $(options.containerSelector).html(html);
+          }
+        })
     }
 };
 
